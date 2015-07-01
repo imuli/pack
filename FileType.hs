3708484fc34cc92f4931b13102198009dde7043b
Imuli <i@imu.li>
2015-07-01 01:06:32 -0400

    merged FileType show and read

--- FileType.hs
+++ FileType.hs
@@ -1,25 +1,44 @@
-module FileType where
+module FileType ( FileType(..)
+                , normFiletype
+                )
+  where
 
 import Data.Char
 import Data.List
-import Text.Read
-import Text.ParserCombinators.ReadP (string, munch)
+import Data.Maybe
+import Text.Read (Read(..), lift)
+import Text.ParserCombinators.ReadP (ReadP, (<++), char, choice, many, munch, sepBy, string)
 
 data FileType = Unknown | GZIP | BZIP2 | LZMA | XZ | TAR | ZIP
-        deriving (Show, Enum)
+        deriving (Enum, Eq)
+
+names :: [(FileType, String)]
+names = [ (BZIP2, "bz2")
+        , (BZIP2, "bzip2")
+        , (GZIP, "gz")
+        , (GZIP, "gzip")
+        , (LZMA, "lzma")
+        , (XZ, "xz")
+        , (TAR, "tar")
+        , (ZIP, "zip")
+        ]
+
+instance Show FileType where
+    show x = case listToMaybe $ filter (\(y, _) -> x == y) names of
+                  Just (_, name) -> name
+                  Nothing -> "Unknown"
+    showList x = showString $ intercalate "." $ map show x
+
+readP :: ReadP FileType
+readP = (choice $ map valueString names)
+        <++ allElse Unknown
+  where
+    valueString (x, y) = string y >> return x
+    allElse y = munch (\_ -> True) >> return y
 
 instance Read FileType where
-    readPrec = (choice $ map strVal [ ("octet-stream", Unknown)
-                                    , ("bzip2", BZIP2)
-                                    , ("gzip", GZIP)
-                                    , ("lzma", LZMA)
-                                    , ("xz", XZ)
-                                    , ("tar", TAR)
-                                    , ("zip", ZIP)
-                                    ]) <++ allElse Unknown
-      where
-        strVal (x, y) = lift $ string x >> return y
-        allElse y = lift $ munch (\_ -> True) >> return y
+    readPrec = lift $ readP
+    readListPrec = lift $ sepBy readP (char '.')
 
 normFiletype :: String -> String
 normFiletype filetype = case map toLower filetype of
