38c42372e899d192157f19a6b068bb630c1997ac
Imuli <i@imu.li>
2015-07-03 11:08:55 -0400

    Archive Identification

--- src/Archive.hs
+++ src/Archive.hs
@@ -1,9 +1,11 @@
 module Archive ( extract
                , build
+               , identify
                , ArchiveType(..)
 ) where
 
 import Archive.Extract
 import Archive.Build
+import Archive.Identify
 import Archive.Types
 
